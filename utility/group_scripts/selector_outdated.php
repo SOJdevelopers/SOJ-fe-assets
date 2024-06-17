7a400c71e4b976e4c739ef97d6e23decd02e1941
wangzhifang001 <3144749507@qq.com>
2024-06-17 20:51:33 +0800

    sync with local

--- utility/group_scripts/selector_outdated.php
+++ utility/group_scripts/selector_outdated.php
@@ -1,4 +1,4 @@
 <?php
-$ret = DB::selectAll("select username from user_info where unix_timestamp(latest_login) < unix_timestamp(DATE_SUB(now(), INTERVAL 180 DAY))");
+$ret = DB::selectAll("select username from user_info where (unix_timestamp(latest_login) < unix_timestamp(DATE_SUB(now(), INTERVAL 180 DAY))) or username not in (select username from contests_registrants)");
 $result = json_encode($ret);
 ?>
